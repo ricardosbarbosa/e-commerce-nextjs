cc8ba7e37103b187e1554d9835d61daf8804369b
Ricardo Barbosa <>
2026-05-19 15:21:46 -0300

    add @vercel/analytics package and integrate Analytics component into layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist_Mono, Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const inter = Inter({
@@ -31,6 +32,7 @@ export default function RootLayout({
       <body className="h-full flex flex-col">
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
