e5bac0a945269b14442627916356e103a9efc455
Ricardo Barbosa <>
2026-05-24 14:42:48 -0300

    add additional image host configurations to Next.js config for enhanced image loading capabilities

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,18 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "tailwindcss.com",
       },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+      },
+      {
+        protocol: "https",
+        hostname: "https://u7zjotnwf20vfcup.public.blob.vercel-storage.com",
+      },
     ],
   },
 };
